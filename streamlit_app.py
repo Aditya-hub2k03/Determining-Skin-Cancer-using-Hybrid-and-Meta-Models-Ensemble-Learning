ee902385d0bb173553f56e13a486a71d7ac593d1
Aditya_2K03 <85829277+Aditya-hub2k03@users.noreply.github.com>
2026-03-03 21:16:38 +0530

    Fix indentation in YOLOv5 class constructor

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ import os
 class YOLOv5(nn.Module): 
     def __init__(self):
         super(YOLOv5, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1) 
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
